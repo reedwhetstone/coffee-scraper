9deb1e8c5f7f25c8160e922a1c0e63e8d4d4f935
Reed Whetstone <rwhetstone0934@gmail.com>
2025-02-19 23:19:44 -0700

    update run-scraper

--- run-scraper.sh
+++ run-scraper.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/ubuntu/coffee-scraper
 /usr/bin/npm run scrape all > scraper.log 2>&1
-cat scraper.log | mail -s "Coffee Scraper Log $(date '+\%Y-\%m-\%d')" rwhetstone0934@gmail.com -A scraper.log
+cat scraper.log | msmtp -a gmail rwhetstone0934@gmail.com
